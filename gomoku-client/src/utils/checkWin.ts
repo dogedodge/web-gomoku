bcffe5db65ef7b03199ba33baeab17b4e7ba110b
dogedodge <dogedodge@mail.com>
2024-02-01 19:54:15 +0000

    fix husky

--- gomoku-client/src/utils/checkWin.ts
+++ gomoku-client/src/utils/checkWin.ts
@@ -2,19 +2,16 @@ import { GoState } from "../constant";
 
 export function checkWin(goStateMap: GoState[][]) {
   // check vertical
-  const verticalStr = goStateMap.map(column=>column.join('')).join('x');
-  if(/1{5}/.test(verticalStr)){
-    console.log('black win');
+  const verticalStr = goStateMap.map((column) => column.join("")).join("x");
+  if (/1{5}/.test(verticalStr)) {
+    console.log("black win");
     return GoState.BLACK;
   }
 
-  if(/2{5}/.test(verticalStr)){
-    console.log('white win');
-    return GoState.WHITE
+  if (/2{5}/.test(verticalStr)) {
+    console.log("white win");
+    return GoState.WHITE;
   }
-
-
-
 
   return GoState.NONE;
 }
