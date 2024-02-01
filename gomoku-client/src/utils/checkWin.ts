298c8b0fcb4ad565d3f2a6d618ac12952401bb1f
dogedodge <dogedodge@mail.com>
2024-02-01 20:04:51 +0000

    check horizontal win

--- gomoku-client/src/utils/checkWin.ts
+++ gomoku-client/src/utils/checkWin.ts
@@ -1,17 +1,40 @@
 import { GoState } from "../constant";
 
 export function checkWin(goStateMap: GoState[][]) {
-  // check vertical
   const verticalStr = goStateMap.map((column) => column.join("")).join("x");
-  if (/1{5}/.test(verticalStr)) {
+  const horizontalStr = transposeMatrix(goStateMap)
+    .map((column) => column.join(""))
+    .join("x");
+  const boardStr = [verticalStr, horizontalStr].join("x");
+
+  if (/1{5}/.test(boardStr)) {
     console.log("black win");
     return GoState.BLACK;
   }
 
-  if (/2{5}/.test(verticalStr)) {
+  if (/2{5}/.test(boardStr)) {
     console.log("white win");
     return GoState.WHITE;
   }
 
   return GoState.NONE;
+}
+
+function transposeMatrix<T>(matrix: T[][]): T[][] {
+  const numRows = matrix.length;
+  const numCols = matrix[0].length;
+
+  // Initialize a new empty matrix with switched dimensions
+  const transposedMatrix = new Array(numCols)
+    .fill(null)
+    .map(() => new Array(numRows));
+
+  // Fill the transposed matrix by swapping rows and columns
+  for (let i = 0; i < numRows; i++) {
+    for (let j = 0; j < numCols; j++) {
+      transposedMatrix[j][i] = matrix[i][j];
+    }
+  }
+
+  return transposedMatrix;
 }
