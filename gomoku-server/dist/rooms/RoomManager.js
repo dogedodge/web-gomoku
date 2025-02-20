1b720a8aea10dad16b8980f1f6da6da103511e64
dogedodge <dogedodge@mail.com>
2025-02-20 21:37:08 +0800

    remove dist

--- gomoku-server/dist/rooms/RoomManager.js
+++
@@ -1,2 +0,0 @@
-"use strict";
-//# sourceMappingURL=RoomManager.js.map
--- gomoku-server/dist/server.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const WSServer_1 = require("./websocket/WSServer");
-const SERVER_PORT = 3030;
-new WSServer_1.WSServer(SERVER_PORT);
-//# sourceMappingURL=server.js.map
