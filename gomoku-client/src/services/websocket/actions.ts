5cb4217f64708a6c519a17defb2f4afe94e5acee
dogedodge <dogedodge@mail.com>
2025-02-22 16:54:42 +0800

    add more actions

--- gomoku-client/src/services/websocket/actions.ts
+++ gomoku-client/src/services/websocket/actions.ts
@@ -3,3 +3,5 @@
 import { createAction } from "@reduxjs/toolkit";
 
 export const wsConnect = createAction("WS_CONNECT");
+export const wsSend = createAction<{ data: unknown }>("WS_SEND");
+export const wsDisconnect = createAction("WS_DISCONNECT");
