30ea7ccc3d07be5398380d1b8c8d02e7c45ccb61
dogedodge <dogedodge@mail.com>
2025-02-22 16:47:26 +0800

    integrate roomSlice in store

--- gomoku-client/src/app/store.ts
+++ gomoku-client/src/app/store.ts
@@ -12,11 +12,13 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import gameSlice from "../features/online/slices/syncSlice"; // Adjust the import path if necessary
 import { createSocketMiddleware } from "../services/websocket/middleware";
+import roomSlice from "../features/online/slices/roomSlice";
 // import { createSocketMiddleware } from "../../services/websocket/middleware"; // Adjust the import path if necessary
 
 // Combine all reducers
 const rootReducer = combineReducers({
   game: gameSlice,
+  room: roomSlice,
   // Add other reducers here if needed
 });
 
